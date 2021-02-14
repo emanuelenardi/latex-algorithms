e2bd6d07e05e802b57662481f88745c85f02ea90
emanuelenardi <nardiemanuele6@gmail.com>
2021-02-14 20:54:24 +0100

    full main script

--- src/scripts/unix/compile-main.sh
+++ src/scripts/unix/compile-main.sh
@@ -6,8 +6,8 @@ INITIAL_WORKING_DIRECTORY=$(pwd)
 # MESSAGE="\n==================== COMPILO DOCUMENTO PRINCIPALE ====================" ; simple_blue_echo
 cd $SRC_DIR
 arara -v main
-# cd $BUILD_DIR
-# find . -type f ! -name '*.pdf' ! -name '.gitignore' | xargs -r rm
-# rm -r _minted-*
+cd $BUILD_DIR
+find . -type f ! -name '*.pdf' ! -name '.gitignore' | xargs -r rm
+rm -r _minted-*
 
 cd $INITIAL_WORKING_DIRECTORY
