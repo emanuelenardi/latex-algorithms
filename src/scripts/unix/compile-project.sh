fd61f8ecca19396140803df2156d26b722687a6d
emanuelenardi <nardiemanuele6@gmail.com>
2021-03-22 18:46:38 +0100

    🐛 FIX: correctly sourcing bash script

--- src/scripts/unix/compile-project.sh
+++ src/scripts/unix/compile-project.sh
@@ -12,7 +12,8 @@ else
 fi
 
 source "${BASH_SOURCE%/*}/compile-preambles.sh"
-source "${BASH_SOURCE%/*}/compile-chapters.sh"
+# NOTE for debugging purposes
+# source "${BASH_SOURCE%/*}/compile-chapters.sh"
 source "${BASH_SOURCE%/*}/compile-main.sh"
 
 cd $INITIAL_WORKING_DIRECTORY
--- src/scripts/unix/tag-main.sh
+++ src/scripts/unix/tag-main.sh
@@ -1,61 +1,64 @@
 #!/bin/bash
 source "${BASH_SOURCE%/*}/common.sh"
 
-getDocumentVersion() {
-    documentVersion=$1
-    echo "nuova versione: $documentVersion"
+print_var () {
+    var=$1
+    printf '%-14s %s\n' "$var:" "${!var}"
 }
 
-print_var () { var=$1 ; printf '%-14s %s\n' "$var:" "${!var}"; }
+setDocumentVersion() {
+    documentVersion=$1
+    echo "nuova versione: $documentVersion"
 
-INITIAL_WORKING_DIRECTORY=$(pwd)
+    INITIAL_WORKING_DIRECTORY=$(pwd)
 
-cd $BUILD_DIR
-MAIN_DOC=$(find -name "main*.pdf")
-
-echo ""
-if [[ -f "$MAIN_DOC" ]]; then
-    main_name=$(find -name "main*.pdf" | cut -f 2 -d '/')
-    # print_var "main_name"
-    main_basename="${main_name%.*}"
-    # print_var "main_basename"
-    main_tagged="$main_basename-tagged.tex"
-    # print_var "main_tagged"
-else 
-    echo "main document does not exist."
-    exit
-fi
-# echo ""
-
-echo "Copio $main_tagged nella cartella build."
-if [[ ! -f "$BUILD_DIR/$main_tagged" ]]; then
-    cd $SCRIPTS_DIR
-
-    local_tex=${0%.*}.tex # stesso nome dello script
-    cp "$local_tex" "$BUILD_DIR/$main_tagged"
-    
     cd $BUILD_DIR
-else 
-    echo "$main_tagged document already exists."
-fi
+    MAIN_DOC=$(find -name "main*.pdf")
 
-echo "Aggiorno versione documento." && echo ""
-sed -i "s/VERSIONE/${documentVersion}/g" "$main_tagged"
+    echo ""
+    if [[ -f "$MAIN_DOC" ]]; then
+        main_name=$(find -name "main*.pdf" | cut -f 2 -d '/')
+        # print_var "main_name"
+        main_basename="${main_name%.*}"
+        # print_var "main_basename"
+        main_tagged="$main_basename-tagged.tex"
+        # print_var "main_tagged"
+    else 
+        echo "main document does not exist."
+        exit
+    fi
+    # echo ""
 
-echo "Compilo il nuovo documento."
-arara -v $main_tagged
-main_tagged="${main_tagged%.*}.pdf"
+    echo "Copio $main_tagged nella cartella build."
+    if [[ ! -f "$BUILD_DIR/$main_tagged" ]]; then
+        cd $SCRIPTS_DIR
 
-echo "" && "Rimuovo file ausiliari."
-find . -type f ! -name '*.pdf' ! -name '.gitignore' | xargs -r rm
+        local_tex=${0%.*}.tex # stesso nome dello script
+        cp "$local_tex" "$BUILD_DIR/$main_tagged"
+        
+        cd $BUILD_DIR
+    else 
+        echo "$main_tagged document already exists."
+    fi
 
-echo "Sostitusco $main_tagged con $main_name."
-if [[ -z "$GITHUB_ACTIONS" ]] ; then
-    rm "$main_name"
-    mv "$main_tagged" "$main_name"
-else
-    sudo rm "$main_name"
-    sudo mv "$main_tagged" "$main_name"
-fi
+    echo "Aggiorno versione documento." && echo ""
+    sed -i "s/VERSIONE/${documentVersion}/g" "$main_tagged"
 
-cd $INITIAL_WORKING_DIRECTORY
+    echo "Compilo il nuovo documento."
+    arara -v $main_tagged
+    main_tagged="${main_tagged%.*}.pdf"
+
+    echo "" && "Rimuovo file ausiliari."
+    find . -type f ! -name '*.pdf' ! -name '.gitignore' | xargs -r rm
+
+    echo "Sostitusco $main_tagged con $main_name."
+    if [[ -z "$GITHUB_ACTIONS" ]] ; then
+        rm "$main_name"
+        mv "$main_tagged" "$main_name"
+    else
+        sudo rm "$main_name"
+        sudo mv "$main_tagged" "$main_name"
+    fi
+
+    cd $INITIAL_WORKING_DIRECTORY
+}
