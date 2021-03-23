cb74716e53aba2be2c9495a0d40849e92083bfc2
emanuelenardi <nardiemanuele6@gmail.com>
2021-03-23 06:12:22 +0100

    👌 IMPROVE: remove unnecessary code

--- src/scripts/unix/compile-project.sh
+++ src/scripts/unix/compile-project.sh
@@ -3,12 +3,8 @@ source "${BASH_SOURCE%/*}/common.sh"
 
 INITIAL_WORKING_DIRECTORY=$(pwd)
 
-# GITHUB_ACTIONS: Always set to true when GitHub Actions is running the workflow.
-# You can use this variable to differentiate when tests are being run locally or by GitHub Actions.
 if [[ -z "$GITHUB_ACTIONS" ]] ; then
   source "${BASH_SOURCE%/*}/reset-project.sh"
-else
-  : # no-op command
 fi
 
 source "${BASH_SOURCE%/*}/compile-preambles.sh"
