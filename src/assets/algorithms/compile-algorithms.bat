d3a492ce138fdecdac45d23d27e2cbc2be67fd6d
emanuelenardi <nardiemanuele6@gmail.com>
2021-04-03 09:48:53 +0200

    👌 IMPROVE: compile all algorithms

--- src/assets/algorithms/compile-algorithms.bat
+++ src/assets/algorithms/compile-algorithms.bat
@@ -1,17 +1,16 @@
 @echo off
-rem https://stackoverflow.com/questions/33276921
-rem https://stackoverflow.com/questions/23228983
+@REM SEE https://stackoverflow.com/questions/33276921
+@REM SEE https://stackoverflow.com/questions/23228983
 
 set back="%cd%"
 
-rem set chapters=02 04 05 06 08 09 10 11 12 13 14 15 16 19 searching sorting
-set chapters=05
+set chapters=02 04 05 06 08 09 10 11 12 13 14 15 16 19 searching sorting
 
 for /d %%d in (%chapters%) do (
     echo(
     echo ========================= CAPITOLO %%d
     cd "%~dp0%%d"
-    latexmk -c
+    latexmk -C
 
     for %%f in (*.tex) do (
         echo(
